8a9433e3f681df32e7a03052391aa6c640d03f17
Edlog <edgar.logz@gmail.com>
2022-04-10 16:26:37 -0300

    update

--- TestePlano.js
+++ TestePlano.js
@@ -1,23 +1,73 @@
 var planos;
 var precos;
 
-function botaoCalcula() {
-    var idade = new Array();
-    var nBeneficiarios = +$("#nBeneficiarios").val();
-    var indexPlano = +$("#plano").val() - 1;
-    var planoSelecionado = planos[indexPlano];
+function selecionaPreco(objArray,n){
+    let result;
 
-    var precoPlano = precos.filter(x => { return x.codigo === planoSelecionado.codigo; });
-
-    console.log("indexPlano: " + indexPlano);
-    console.log("Número de Beneficiários: " + nBeneficiarios);
-    console.log(planoSelecionado);
-    console.log(precoPlano);
-
-    for (let i = 0; i < nBeneficiarios; i++) {
-
+    for(let i = 0; i < objArray.length;i++){
+        if(objArray[i].minimo_vidas <= n){
+            result = objArray[i];
+        }
     }
 
+    return result;
+}
+
+function botaoCalcula() {
+    let idades = new Array();
+    let html = "";
+    let nBeneficiarios = +$("#nBeneficiarios").val();
+    let indexPlano = +$("#plano").val() - 1;
+    let planoSelecionado = planos[indexPlano];
+    let precoPlano = precos.filter(x => { return x.codigo === planoSelecionado.codigo; });
+    let precoTotal =  0;
+    let elem = $("#resultPrecos").empty();
+
+    precoPlano.sort((a, b) => {
+        return a.minimo_vidas - b.minimo_vidas;
+    });
+
+    let precoFinal = selecionaPreco(precoPlano,nBeneficiarios);
+    let faixa = 0;
+
+    for (let i = 0; i < nBeneficiarios; i++) {
+        idades.push($("#nIdade"+i).val());
+        if(idades[i] < 18){
+            faixa = precoFinal.faixa1;
+        }else if(idades[i] > 17 && idades[i] < 41){
+            faixa = precoFinal.faixa2;
+        }else{
+            faixa = precoFinal.faixa3;
+        }
+
+        precoTotal += faixa;
+
+        console.log(idades[i]);
+        console.log(parseFloat(faixa.toFixed(2)));
+
+        const precoFormatado  = new Intl.NumberFormat(`pt-BR`, {
+            currency: `BRL`,
+            style: 'currency',
+        }).format(faixa);
+
+        html = `
+            <div>
+                <span>Idade: ` + idades[i] + ` - ` + precoFormatado + `</span>
+            </div>
+        `;
+        elem.append(html);
+    }
+
+    const precoTotalForm  = new Intl.NumberFormat(`pt-BR`, {
+        currency: `BRL`,
+        style: 'currency',
+    }).format(precoTotal);
+
+    html = `<div style="text-align: center ">
+                <span>Preço Total: ` + precoTotalForm + `</span>
+            </div>`;
+    
+    elem.append(html);
 }
 
 $(document).ready(function () {
@@ -54,8 +104,6 @@ $(document).ready(function () {
         let qtd = 4;
         let html = "<option disabled selected value=0> -- Selecione Uma Opção -- </option>";
         let elem = $("#nBeneficiarios");
-
-        console.log(planos);
 
         for (let i = 0; i < qtd; i++) {
             html += `
